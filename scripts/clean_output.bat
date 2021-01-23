f455134e7639a8e3bc4c8f800ce636d54d1224b3
zonarmr <zonarmr@gmail.com>
2021-01-23 00:10:22 -0600

    v2.1

--- scripts/clean_output.bat
+++ scripts/clean_output.bat
@@ -1,33 +1,35 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :::                                                    :::
 :::          Carliv Image Kitchen for Android          :::
-:::   boot+recovery images copyright-2016 carliv@xda   :::
+:::  boot ^& recovery images copyright-2020 carliv.eu   :::
 :::   including support for MTK powered phones images  :::
 :::                                                    :::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo off
 cd "%~dp0"
-IF EXIST "%~dp0\bin" SET PATH=%PATH%;"%~dp0\bin"
+IF EXIST "%~dp0\bin" SET PATH="%~dp0\bin";%PATH%
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 Setlocal EnableDelayedExpansion
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo(    
+ecco {1B}
 echo ***************************************************
 echo *                                                 *
-cecho *      {0B}Carliv Image Kitchen for Android{#} v1.3      *{\n}
-cecho *     boot+recovery images (c)2016 {0B}carliv@xda{#}     *{\n}
-cecho * including support for {0E}MTK powered {#}phones images *{\n}
-cecho *               {0A}WINDOWS x64 {#}version               *{\n}
-echo ***************************************************
+echo *      Carliv Image Kitchen for Android v2.1      *
+echo *    boot ^& recovery images (c)2020 carliv.eu     *
+echo * including support for MTK powered phones images *
+echo *               WINDOWS x64 version               *
+echo *                                                 *
+ecco ***************************************************{0F}{\n}
 echo *           Cleaning the output folder            *
 echo ***************************************************
 echo(
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-for /d %%d in ("%~dp0\output\*") do rd /s /q "%%d" >nul
-for /f %%a in ("%~dp0\output\*") do del /q "%%a" >nul 
+for /d %%d in ("%~dp0\output\*") do rd /s /q "%%d" >nul 2>&1
+for /f %%a in ("%~dp0\output\*") do del /q "%%a" >nul 2>&1 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 echo(
-cecho {0E}The output folder is clean now!{#}{\n}
+ecco {0E}The output folder is clean now!{#}{\n}
 goto end
 echo(
 :end
--- scripts/unpack_img.bat
+++ scripts/unpack_img.bat
@@ -1,23 +1,25 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :::                                                    :::
 :::          Carliv Image Kitchen for Android          :::
-:::   boot+recovery images copyright-2016 carliv@xda   :::
+:::  boot ^& recovery images copyright-2020 carliv.eu   :::
 :::   including support for MTK powered phones images  :::
 :::                                                    :::
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo off
 cd "%~dp0"
-IF EXIST "%~dp0\bin" SET PATH=%PATH%;"%~dp0\bin"
+IF EXIST "%~dp0\bin" SET PATH="%~dp0\bin";%PATH%
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 Setlocal EnableDelayedExpansion
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::       
+ecco {1B}
 echo ***************************************************
 echo *                                                 *
-cecho *      {0B}Carliv Image Kitchen for Android{#} v1.3      *{\n}
-cecho *     boot+recovery images (c)2016 {0B}carliv@xda{#}     *{\n}
-cecho * including support for {0E}MTK powered {#}phones images *{\n}
-cecho *               {0A}WINDOWS x64 {#}version               *{\n}
-echo ***************************************************
+echo *      Carliv Image Kitchen for Android v2.1      *
+echo *    boot ^& recovery images (c)2020 carliv.eu     *
+echo * including support for MTK powered phones images *
+echo *               WINDOWS x64 version               *
+echo *                                                 *
+ecco ***************************************************{0F}{\n}
 echo *           The unpacking images script           *
 echo ***************************************************
 echo(
@@ -26,12 +28,12 @@ if "%~1" == "" goto noinput
 if "%~2" == "" goto noinput
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 set "folder=%~n2"
-cecho Your image:{0E} %folder%.img {#}{\n}
+ecco Your image:{0E} %folder%.img {#}{\n}
 set "file=%~nx1"
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :unpack
 echo(
-cecho Create the{0E} %~n2 {#}folder.{\n}
+ecco Create the{0E} %~n2 {#}folder.{\n}
 echo(
 if exist "%folder%" rd /s/q "%folder%"
 md %folder%
@@ -41,7 +43,7 @@ unpackbootimg -i %file% -o %folder%
 :donecheck
 cd %folder%
 for %%a in ("%file%-ramdisk.*") do set ext=%%~xa
-cecho Compression used:{0E} %ext:~1% {#}{\n}
+ecco Compression used:{0E} %ext:~1% {#}{\n}
 type nul > %file%-ramdisk-compress
 echo %ext:~1% > "%file%-ramdisk-compress"
 echo(
@@ -60,7 +62,7 @@ cd ..\
 del "%file%-ramdisk.gz"
 cd ..\
 echo(
-cecho Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
+ecco Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :lzma
@@ -71,7 +73,7 @@ cd ..\
 del "%file%-ramdisk.lzma"
 cd ..\
 echo(
-cecho Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
+ecco Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :xz
@@ -82,7 +84,7 @@ cd ..\
 del "%file%-ramdisk.xz"
 cd ..\
 echo(
-cecho Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
+ecco Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :bz2
@@ -93,7 +95,7 @@ cd ..\
 del "%file%-ramdisk.bz2"
 cd ..\
 echo(
-cecho Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
+ecco Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :lz4
@@ -104,7 +106,7 @@ cd ..\
 del "%file%-ramdisk.lz4"
 cd ..\
 echo(
-cecho Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
+ecco Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :lzo
@@ -115,18 +117,18 @@ cd ..\
 del "%file%-ramdisk.lzo"
 cd ..\
 echo(
-cecho Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
+ecco Done. Your image is unpacked in{0E} %folder% {#}folder.{\n}
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :ziperror
 echo(
-cecho {0C}Your ramdisk archive is corrupt or unknown format. Exit script.{#}{\n}
+ecco {0C}Your ramdisk archive is corrupt or unknown format. Exit script.{#}{\n}
 echo(
 goto end
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :noinput
 echo(
-cecho {0C}No image file selected. Exit script.{#}{\n}
+ecco {0C}No image file selected. Exit script.{#}{\n}
 echo(
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :end
